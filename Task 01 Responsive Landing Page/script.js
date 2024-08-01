4a549418b51258d3aeaee8cccdfbe93efbc0cca8
Jeevan Harishchandra Darekar <123078281+Jeevan8807@users.noreply.github.com>
2024-08-01 12:14:11 +0530

    Delete Task 01 Responsive Landing Page directory

--- Task 01 Responsive Landing Page/script.js
+++
@@ -1,9 +0,0 @@
-const nav = document.querySelector('.fixed-nav');
-
-window.addEventListener('scroll', function () {
-  if (window.scrollY > 100) { // Change scroll threshold as needed
-    nav.classList.add('scrolled');
-  } else {
-    nav.classList.remove('scrolled');
-  }
-});
